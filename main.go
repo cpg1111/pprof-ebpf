877d8f9b6247d16744129e68b6904926fe50b799
Christian Grabowski <christiang11754@gmail.com>
2018-06-15 02:42:17 -0400

    force sudo

--- main.go
+++ main.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/cpg1111/pprof-ebpf/cmd"
 )
 
 func main() {
+	if os.Getuid() != 0 {
+		cmd := exec.Command("sudo", os.Args...)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			os.Exit(1)
+		}
+	}
 	fmt.Println("running pprof-ebpf")
 	cmd.Execute()
 }
